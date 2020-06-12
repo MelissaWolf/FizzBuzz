f8e447ff314d600af8413eec5ed1fbf7bfa1efcc
Melis <Melis@LAPTOP-REIR8099>
2020-06-12 14:13:43 +1200

    Add project files.

---
+++ FizzBuzz/MainForm.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace FizzBuzz
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.TotMoneyBox = new System.Windows.Forms.TextBox();
+            this.HundredBox = new System.Windows.Forms.TextBox();
+            this.FiftyBox = new System.Windows.Forms.TextBox();
+            this.TwentyBox = new System.Windows.Forms.TextBox();
+            this.TenBox = new System.Windows.Forms.TextBox();
+            this.FiveBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.OneBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.MoneyBtn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TwoBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 16;
+            this.ListBox.Location = new System.Drawing.Point(619, 22);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(185, 388);
+            this.ListBox.TabIndex = 0;
+            // 
+            // GoButton
+            // 
+            this.GoButton.Location = new System.Drawing.Point(474, 22);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(121, 107);
+            this.GoButton.TabIndex = 1;
+            this.GoButton.Text = "FizzBuzz";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // TotMoneyBox
+            // 
+            this.TotMoneyBox.Location = new System.Drawing.Point(142, 40);
+            this.TotMoneyBox.Name = "TotMoneyBox";
+            this.TotMoneyBox.Size = new System.Drawing.Size(178, 22);
+            this.TotMoneyBox.TabIndex = 2;
+            // 
+            // HundredBox
+            // 
+            this.HundredBox.Location = new System.Drawing.Point(12, 230);
+            this.HundredBox.Name = "HundredBox";
+            this.HundredBox.Size = new System.Drawing.Size(61, 22);
+            this.HundredBox.TabIndex = 3;
+            // 
+            // FiftyBox
+            // 
+            this.FiftyBox.Location = new System.Drawing.Point(89, 230);
+            this.FiftyBox.Name = "FiftyBox";
+            this.FiftyBox.Size = new System.Drawing.Size(61, 22);
+            this.FiftyBox.TabIndex = 5;
+            // 
+            // TwentyBox
+            // 
+            this.TwentyBox.Location = new System.Drawing.Point(166, 230);
+            this.TwentyBox.Name = "TwentyBox";
+            this.TwentyBox.Size = new System.Drawing.Size(61, 22);
+            this.TwentyBox.TabIndex = 7;
+            // 
+            // TenBox
+            // 
+            this.TenBox.Location = new System.Drawing.Point(243, 230);
+            this.TenBox.Name = "TenBox";
+            this.TenBox.Size = new System.Drawing.Size(61, 22);
+            this.TenBox.TabIndex = 9;
+            // 
+            // FiveBox
+            // 
+            this.FiveBox.Location = new System.Drawing.Point(321, 230);
+            this.FiveBox.Name = "FiveBox";
+            this.FiveBox.Size = new System.Drawing.Size(61, 22);
+            this.FiveBox.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Total Money";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "100\'s";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "50\'s";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(191, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "20\'s";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(270, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "10\'s";
+            // 
+            // OneBox
+            // 
+            this.OneBox.Location = new System.Drawing.Point(474, 230);
+            this.OneBox.Name = "OneBox";
+            this.OneBox.Size = new System.Drawing.Size(61, 22);
+            this.OneBox.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(348, 201);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 17);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "5\'s";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(509, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 17);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "1\'s";
+            // 
+            // MoneyBtn
+            // 
+            this.MoneyBtn.Location = new System.Drawing.Point(194, 88);
+            this.MoneyBtn.Name = "MoneyBtn";
+            this.MoneyBtn.Size = new System.Drawing.Size(83, 31);
+            this.MoneyBtn.TabIndex = 22;
+            this.MoneyBtn.Text = "Sort Money";
+            this.MoneyBtn.UseVisualStyleBackColor = true;
+            this.MoneyBtn.Click += new System.EventHandler(this.MoneyBtn_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(433, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 17);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "2\'s";
+            // 
+            // TwoBox
+            // 
+            this.TwoBox.Location = new System.Drawing.Point(398, 230);
+            this.TwoBox.Name = "TwoBox";
+            this.TwoBox.Size = new System.Drawing.Size(61, 22);
+            this.TwoBox.TabIndex = 23;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TwoBox);
+            this.Controls.Add(this.MoneyBtn);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.OneBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FiveBox);
+            this.Controls.Add(this.TenBox);
+            this.Controls.Add(this.TwentyBox);
+            this.Controls.Add(this.FiftyBox);
+            this.Controls.Add(this.HundredBox);
+            this.Controls.Add(this.TotMoneyBox);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.ListBox);
+            this.Name = "MainForm";
+            this.Text = "FizzBuzz & Money Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.TextBox TotMoneyBox;
+        private System.Windows.Forms.TextBox HundredBox;
+        private System.Windows.Forms.TextBox FiftyBox;
+        private System.Windows.Forms.TextBox TwentyBox;
+        private System.Windows.Forms.TextBox TenBox;
+        private System.Windows.Forms.TextBox FiveBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox OneBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button MoneyBtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TwoBox;
+    }
+}
+
---
+++ FizzBuzz/MainForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FizzBuzz
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    ListBox.Items.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    ListBox.Items.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    ListBox.Items.Add("Buzz");
+                }
+                else
+                {
+                    ListBox.Items.Add(i);
+                }
+
+            }
+
+        }
+
+        private void MoneyBtn_Click(object sender, EventArgs e)
+        {
+            int change = 0;
+            int prevChange = Int32.Parse(TotMoneyBox.Text);
+            int boxNum = 0;
+
+            int FuncChange(int dollar) {
+                change = prevChange % dollar;
+                boxNum = prevChange - change;
+                prevChange = prevChange - boxNum;
+                boxNum = boxNum / dollar;
+                return(change);
+            }
+
+            FuncChange(100);
+            HundredBox.Text = "" + boxNum;
+
+            FuncChange(50);
+            FiftyBox.Text = "" + boxNum;
+
+            FuncChange(20);
+            TwentyBox.Text = "" + boxNum;
+
+            FuncChange(10);
+            TenBox.Text = "" + boxNum;
+
+            FuncChange(5);
+            FiveBox.Text = "" + boxNum;
+
+            FuncChange(2);
+            TwoBox.Text = "" + boxNum;
+
+            FuncChange(1);
+            OneBox.Text = "" + boxNum;
+
+            /*
+            change = prevChange % 100;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 100;
+            HundredBox.Text = "" + boxNum;
+
+            change = prevChange % 50;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 50;
+            FiftyBox.Text = "" + boxNum;
+
+            change = prevChange % 20;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 20;
+            TwentyBox.Text = "" + boxNum;
+
+            change = prevChange % 10;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 10;
+            TenBox.Text = "" + boxNum;
+
+            change = prevChange % 5;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 5;
+            FiveBox.Text = "" + boxNum;
+
+            change = prevChange % 2;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 2;
+            TwoBox.Text = "" + boxNum;
+
+            change = prevChange % 1;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 1;
+            OneBox.Text = "" + boxNum;
+            */
+
+        }
+    }
+}
